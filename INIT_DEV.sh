eb0f999e5d9581d1f9abec7509bbdec563b31ca3
Trevis Gulby <trevisgulby@protonmail.ch>
2018-01-22 15:33:25 +0100

    INIT_DEV.sh : Correcting typo and adding header

--- INIT_DEV.sh
+++ INIT_DEV.sh
@@ -1,6 +1,10 @@
 #:/usr/bin/env bash
+#   ____  _  _  ____  ____    ____  ____  _  _
+#  (_  _)( \( )(_  _)(_  _)  (  _ \( ___)( \/ )
+#   _)(_  )  (  _)(_   )(     )(_) ))__)  \  /
+#  (____)(_)\_)(____) (__)   (____/(____)  \/
+#################InitDev.sh####################
 
-
-sudo apt-get install nodejs np
+sudo apt-get install nodejs npm
 sudo npm  install -g yarn
 yarn install
