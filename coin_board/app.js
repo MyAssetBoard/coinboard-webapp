1ee0091eefcd99a4abbeab394892e46ebf19c70f
Trevis Gulby <trevisgulby@protonmail.ch>
2018-01-22 16:19:35 +0100

    Added jsdoc commentary to app.js

--- coin_board/app.js
+++ coin_board/app.js
@@ -1,3 +1,4 @@
+/** Depencies import */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,16 +6,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/** index route import */
 var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+/** view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+/** Globla app setup */
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,20 +26,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
-// catch 404 and forward to error handler
+/** catch 404 and forward to error handler */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+/** error handler */
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  /** set locals, only providing error in development */
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  /** render the error page */
   res.status(err.status || 500);
   res.render('error');
 });
