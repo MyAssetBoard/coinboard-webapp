3dc4ee562964b564d2addb9a5c54c84619e132bd
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-06 10:03:07 +0100

    Add json object to params in order to automatically include all in template

--- coin_board/routes/index.js
+++ coin_board/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var param = {
+	title   : "Coin_Board",
+	page	: "index",
+	trade 	: "javascripts/tradestream.js",
+	change 	: "javascripts/changer_api.js"
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Coin_Board' });
+  res.render('index', param);
 });
 
 module.exports = router;
--- coin_board/routes/myassets.js
+++ coin_board/routes/myassets.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var params = {
+	title   : "Coin_Board/My assets",
+	page	: "assets",
+	manage : "javascripts/assets_management.js",
+	scripts	: {
+		manage : "javascripts/assets_management.js"
+	}
+};
 
 /* GET assets page. */
 router.get('/', function(req, res, next) {
-  res.render('assets', { title: 'Coin_Board/My assets' });
+  res.render('assets', params);
 });
 
 module.exports = router;
