4870b02f4477d870c67a3262320308bd9a98e59e
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-06 11:18:13 +0100

    Add a dirname directive to shorten javascripts inclusion and correct typo in comment

--- coin_board/app.js
+++ coin_board/app.js
@@ -18,14 +18,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-/** Globla app setup */
+/** Global app setup */
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/javascripts')));
 
+/** Routes import */
 app.use('/', index);
 app.use('/myassets', myassets);
 
