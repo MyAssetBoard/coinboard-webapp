84fee7c4af102437297b3258eca24d05c4159271
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-08 16:44:19 +0100

    put var in const, add login page methods , add error data model and indent file

--- coin_board/app.js
+++ coin_board/app.js
@@ -7,9 +7,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 /** index route import */
-var index = require('./routes/index');
+const index = require('./routes/index');
 /** Assets route import */
-var myassets = require('./routes/myassets');
+const myassets = require('./routes/myassets');
+/** Login route import */
+const login = require('./routes/login');
+/** Error view params */
+const error_param = require('./models/error_param');
+
 
 var app = express();
 
@@ -22,7 +27,7 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/javascripts')));
@@ -30,23 +35,24 @@ app.use(express.static(path.join(__dirname, 'public/javascripts')));
 /** Routes import */
 app.use('/', index);
 app.use('/myassets', myassets);
+app.use('/login', login);
 
 /** catch 404 and forward to error handler */
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 /** error handler */
 app.use(function(err, req, res, next) {
-  /** set locals, only providing error in development */
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  /** render the error page */
-  res.status(err.status || 500);
-  res.render('error', { title: 'Oh di bug!' });
+	/** set locals, only providing error in development */
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	/** render the error page */
+	res.status(err.status || 500);
+	res.render('error', error_param);
 });
+
 
 module.exports = app;
