bcb9da7d10b69928908b4f603965d36e67e9be4d
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-08 16:42:47 +0100

    refactor routes / add default params for pages as json models

--- coin_board/routes/index.js
+++ coin_board/routes/index.js
@@ -1,19 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var param = {
-	title	: "Coin_Board",
-	author	: "© Copyright 2018 coin_board",
-	page	: "index",
-	scripts	: {
-		trade 	: "tradestream.js",
-		change 	: "changer_api.js"
-	},
-	blocks : {
-		jumbo		: "blocks/my_jumbotron",
-		ticker		: "blocks/ticker_panel",
-		livestream	: "blocks/tradestream"
-	}
-};
+var param = require('../models/index_param');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
