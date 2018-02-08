446ce1f62928531b91cd859f22fd18721e38ef0b
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-08 16:25:45 +0100

    add a redirect when bug method for error pages

---
+++ coin_board/public/javascripts/to_homepage.js
@@ -0,0 +1,7 @@
+console.log('U reach a bug, see page source for details' + '\n\n'
++ '--> Help Fightings bugs by making a report on our Bitbucket repo' + '\n');
+
+t1 = window.setTimeout(function() {window.location = "/";}, 10000);
+console.log('\n\tRedirecting to homepage in 10s...');
+
+t1;
