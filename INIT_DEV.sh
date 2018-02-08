dc79d9b032e7ad567071c3d3ff341fd8aea91760
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-08 12:52:36 +0100

    Add 3 function (wrapper arround pm2)for start restart & kill app  + put an usage message on rethinkdb deprecation (@TODO : mongo setup)  + separate execution tasks on app_startDev

--- INIT_DEV.sh
+++ INIT_DEV.sh
@@ -14,9 +14,17 @@ export PM2="./node_modules/pm2/bin/pm2"
 export PM2DEV="./node_modules/pm2/bin/pm2-dev"
 # coin_board app
 export APPCONF="./conf/webfront.pm2conf.json"
+export WVSERV="webview.service"
+export DBSERV="mongodb.service"
+export WSSERV="websocket.service"
+
+#External ressources
+export TICKERURL="https://raw.githubusercontent.com/crypti/cryptocurrencies/master/"
+export TICKER="cryptocurrencies.json"
 #Color
-BLD="\033[1m"
+BLD="\033[40m"
 CL="\033[0m"
+RD="\033[101m"
 
 function usage ()
 {
@@ -25,17 +33,19 @@ function usage ()
 
 	$BLD Usage :$CL INIT_DEV [-b] [-c] [-d] [-r] [-t] [-h/-u]
 
-	-b:$BLD Setup$CL your rethinkdb dev environnement (run only once)
-	-c:$BLD Setup$CL your nodejs dev environnement (run only once)
-	-d:$BLD Generate$CL html doc with jsdoc
-	-r:$BLD reset$CL database tmpdata and restart app in test mode
-	-s:$BLD run$CL app with pm2 (production env)
-	-t:$BLD run$CL app in dev mode (watch for file change)
-	-h:$BLD show$CL this help text and exit
+	-b:$BLD.Setup$CL your rethinkdb dev environnement$RD*$CL $BLD**$CL
+	-c:$BLD.Setup$CL your nodejs dev environnement$BLD**$CL
+	-d:$BLD.Generate$CL html doc with jsdoc
+	-r:$BLD.reset$CL database tmpdata and restart app in test mode
+	-s:$BLD.run$CL app with pm2 (production env)
+	-t:$BLD.run$CL app in dev mode (watch for file change)
+	-h:$BLD.show$CL this help text and exit
 
 	Notes : Accept only$BLD one$CL arg.
 		$BLD[-b]$CL options currently supported system are fedora / centos and
 		ubuntu / debian.
+	$RD*DEPRECATED$CL : The stack is now rolling with mongo
+	** : run only once
 	";
 }
 
@@ -74,7 +84,12 @@ function dbsetup ()
 
 function app_startDev ()
 {
-	exec $PM2DEV start $APPCONF
+	#exec $PM2DEV start $APPCONF
+	# order matters !
+	mongod -f conf/mongodb.conf &
+	#$PM2 start $APPCONF --only "$DBSERV";
+	$PM2 start $APPCONF --only "$WSSERV";
+	$PM2 start $APPCONF --only "$WVSERV";
 }
 
 function app_startProd ()
@@ -85,8 +100,20 @@ function app_startProd ()
 function reset_db ()
 {
 	rm -rf tmpdata && mkdir tmpdata
-	wget https://raw.githubusercontent.com/crypti/cryptocurrencies/master/cryptocurrencies.json \
-	-O tmpdata/cryptocurrencies.json && app_startDev
+	wget "$TICKERURL/$TICKER" \
+	-O "tmpdata/$TICKER" && app_startDev
+}
+
+function app_kill ()
+{
+	$PM2 kill
+	killall -9 mongod;
+}
+
+function app_reload ()
+{
+	app_kill
+	app_startDev
 }
 
 function genddoc ()
@@ -144,6 +171,16 @@ do
 
 		'n-t')
 		app_startDev
+		exit 0
+		;;
+
+		'n-k')
+		app_kill
+		exit 0
+		;;
+
+		'n-rl')
+		app_reload
 		exit 0
 		;;
 
