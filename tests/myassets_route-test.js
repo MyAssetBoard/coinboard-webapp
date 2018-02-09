45c90fe449792d2cb228828cdc86d8af1ae13f60
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-09 12:02:15 +0100

    add unit test for index , login and myassets routes functions

---
+++ tests/myassets_route-test.js
@@ -0,0 +1,27 @@
+var assert= require('assert');
+var request = require('supertest');
+var test_url = '/myassets';
+var app = require('../coin_board/app');
+
+describe('Simple GET / on app', function() {
+	it('respond with json', function(done) {
+		request(app)
+		.get(test_url)
+		.set('Accept', 'application/json')
+		.expect(200)
+		.end(function(err, res) {
+			if (err) return done(err);
+			done();
+		});
+	});
+	it('respond with 404', function(done) {
+		request(app)
+		.get(test_url + '/foobar')
+		.set('Accept', 'application/json')
+		.expect(404)
+		.end(function(err, res) {
+			if (err) return done(err);
+			done();
+		})
+	})
+});
