cf78c88a9f187432e21c66760422b12734a671a0
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-10 13:22:48 +0100

    add public socket auth script for connecting

---
+++ coin_board/public/javascripts/auth_socket.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	var auth = io.connect('http://localhost:3001/auth');
+
+	auth.on('connection', function (socket) {
+		console.log('succesfully connected to auth socket');
+	});
+	auth.on('my-message', function (data) {
+		console.log(data.msg ? data.msg : data);
+	});
+	auth.on('error-message', function (data) {console.log(data.msg);});
+	$('#click').click(function() {
+		var InputName = $('#InputName').val().trim();
+		if (tosend.length > 2) {
+			console.log('click [' + InputName + '] ');
+			auth.emit('user login', {logname: InputName});
+		}
+	})
+});
