ee12361d3c3baaef78e5bb4d74e9115e6073237d
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-10 12:05:00 +0100

    complete rebuild of websocket micro service, now with rt login check

--- coin_board/bin/our_websocket.js
+++ coin_board/bin/our_websocket.js
@@ -1,43 +1,74 @@
 /**
- * @file Main runable executable for websocket micro service
- * @author based on socket.io doc app and edited by Trevis Gulby
- */
+* @file Main runable executable for websocket micro service
+* @author based on socket.io doc app and edited by Trevis Gulby
+*/
 var async = require('async');
 
 /** PORT to connect to */
 var port = process.env.WSPORT || '3001';
-/** connected state boolean */
-var connected = false;
+var io = require('socket.io')(port);
+console.log('WEBSOCKET - Runnnig');
+/** dep import */
+const crudMod = require('../methods/mongo_crud');
 
-/** Instantiate socket server */
-var app = require('http').createServer().listen(port);
 
-/** WebSocket via socketIO */
-var io = require('socket.io').listen(app);
-/** Data container */
-var nicknames = {};
-var cur_nick = null;
-
-console.log('WEBSOCKET - coin_board micro service started\n');
-
-io.on('error', socketError);
-
-/** Ws connection event */
-io.sockets.on('connection', function (socket) {
-	console.log('one client connection');
-	socket.on('datainput', function (msg) {
-		msg = msg.trim();
-		console.log('data input recived :[' + msg + ']');
+function checkUsr() {
+	return new Promise((resolve, reject) => {
+		dfDb = "test2";
+		dfCo = "users";
+		dfkey = "name";
+		dreq = askeduser.logname;
+		console.log(dreq);
+		var crud = new crudMod("test2");
+		crud.FindInCollection(dfDb, dfCo, dfkey, dreq, function(result) {
+			if (result) {
+				if (result['name']) {
+					resolve(result);
+				} else {
+					var reason = new Error('Bad user');
+					reject(reason);
+				}
+			} else {
+				var reason = new Error('Bad user');
+				reject(reason);
+			}
+		});
 	});
-	socket.on('disconnect', function () {
-		if (!socket.nickname) return;
-		throw error;
+}
+
+function checkData(data) {
+	if (data && data['logname']) {
+		askeduser = data;
+		checkUsr()
+		.then(function(res, err) {
+			if (!res || err) {
+				console.log('no res..');
+				throw(err);
+			}
+			console.log(res);
+			io.of('/auth').emit('my-message', res);
+			return true;
+		})
+		.catch(function (rej, err) {
+			console.error(rej.message);
+			var errmsg = { errcode: 22, msg: rej.message };
+			io.of('/auth').emit('error-message', errmsg);
+			if (err) throw(err);
+			return false;
+		})
+	}
+	return false;
+}
+
+io
+.of('/auth')
+.on('connection', function (socket) {
+	console.log('a user connected with id %s', socket.id);
+	var usrtmp = "welcome usr " + socket.id.replace(/\/auth#/g, '-');;
+	io.of('/auth').emit('my-message', { msg: usrtmp });
+	socket.on('user login', function (data) {
+		if (checkData(data, socket)) {
+			io.of('/auth').emit('my-message', data);
+		}
 	});
 });
-
-
-/** Ws socket error handling */
-function socketError(error) {
-	console.log('Socket thrown error : \n' + JSON.stringify(error));
-	throw error;
-}
