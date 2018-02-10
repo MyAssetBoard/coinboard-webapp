7b050735656fbb81e297a48358a696fec468e356
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-10 13:15:56 +0100

    add param for login rendering

--- coin_board/models/login_param.js
+++ coin_board/models/login_param.js
@@ -1,7 +1,17 @@
+var cdn = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"
+
 const param = {
-	title	: "Coin_Board.|Login",
+	title	: "Login",
 	author	: "© Copyright 2018 coin_board x)",
-	page	: "login"
+	page	: "login",
+	scripts	: {
+		socketio	: cdn,
+		mysocket	: "auth_socket.js"
+	},
+	blocks : {
+		jumbo		: "blocks/my_jumbotron",
+		topblock	: "blocks/login_block"
+	}
 };
 
 module.exports = param;
