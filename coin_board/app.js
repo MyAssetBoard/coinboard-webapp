0efe4df5729915aff50f40d1e8d34bbf4ff0d396
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-10 13:20:13 +0100

    add check header methods and param and various security hardenin

--- coin_board/app.js
+++ coin_board/app.js
@@ -17,6 +17,21 @@ const error_param = require('./models/error_param');
 
 
 var app = express();
+const allowedMethods = ['GET'];
+var options = {
+	'X-Content-Type-Options'	: 'nosniff',
+	'X-Frame-Options'		: 'DENY',
+	'X-XSS-Protection'		: '1; mode=block',
+	'etag'				: 'false'
+}
+var favOptions = {
+	dotfiles: 'ignore',
+	etag: false,
+	extensions: ['htm', 'html'],
+	index: false,
+	maxAge: '1d',
+	redirect: false
+}
 
 /** view engine setup */
 app.set('views', path.join(__dirname, 'views'));
@@ -24,13 +39,16 @@ app.set('view engine', 'ejs');
 
 
 /** Global app setup */
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.disable('x-powered-by');
+
+app.use('/favicon.ico', express.static('images/favicon.ico', favOptions));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {etag: false}));
 app.use(express.static(path.join(__dirname, 'public/javascripts')));
+
 
 /** Routes import */
 app.use('/', index);
@@ -46,12 +64,23 @@ app.use(function(req, res, next) {
 
 /** error handler */
 app.use(function(err, req, res, next) {
+	/** append header param to response */
+	for (var k in options) {
+		if (options.hasOwnProperty(k)) {
+			res.append(k, options[k]);
+		}
+	}
 	/** set locals, only providing error in development */
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 	/** render the error page */
 	res.status(err.status || 500);
-	res.render('error', error_param);
+	/** Check if method is GET or POST only */
+	if (!allowedMethods.includes(req.method)) {
+		res.status(500).send('Not allowed ;)');
+	} else {
+		res.render('error', error_param);
+	}
 });
 
 
