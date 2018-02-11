c6275c9f857caea1ccc22185acbe2939e16fe5b6
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-11 15:25:50 +0100

    add register user methods

--- coin_board/bin/our_websocket.js
+++ coin_board/bin/our_websocket.js
@@ -25,12 +25,51 @@ function checkUsr(data) {
 	});
 }
 
-function checkData(data, socket) {
+function registerUsr(data) {
+	return new Promise((resolve, reject) => {
+		data.InputName = data.InputName.replace(/\W/g, '');
+		data.InputEmail = data.InputEmail.trim();
+		data.InputCompany = data.InputCompany.replace(/\W/g, '');
+		var toRegister = {
+			'username' : data.InputName,
+			'useremail' : data.InputEmail,
+			'companyname' : data.InputCompany,
+			'usercurrency' : data.InputBcurr
+		};
+		var crud = new crudMod("test2");
+		crud.InsertInCollection("r_users", toRegister, function(result) {
+			if (result) {resolve(result)}
+			reject(new Error('Db Error'));
+		});
+	});
+}
+
+function checkRegData(data, socket) {
+	if (data) {
+		if (data['InputName'] && data['InputEmail']
+		&& data['InputCompany'] && data['InputBcurr']) {
+			registerUsr(data)
+			.then(function(res) {
+				io.of('/register').to(socket.id).emit('my-message', res);
+				return true;
+			})
+			.catch(function (rej, err) {
+				console.error(rej.message);
+				var errmsg = { errcode: 22, msg: rej.message };
+				io.of('/register').to(socket.id).emit('error-message', errmsg);
+				if (err) throw(err);
+				return false;
+			})
+
+		}
+	}
+}
+
+function checkcoData(data, socket) {
 	if (data){
 		if (data['logname']) {
 			checkUsr(data)
-			.then(function(res, err) {
-				if (!res || err) { throw(err); }
+			.then(function(res) {
 				io.of('/auth').to(socket.id).emit('my-message', res);
 				return true;
 			})
@@ -50,10 +89,23 @@ io
 .of('/auth')
 .on('connection', function (socket) {
 	connected += 1;
-	console.log('user connected id: %s\nTotal : %i', socket.id, connected);
-	var usrtmp = "welcome usr " + socket.id.replace(/\/auth#/g, '-');
+	var log = socket.id.replace(/\/auth#/g, 'User : ');
+	log += " connected to [/auth] route \nconnected : ";
+	log += connected;
+	console.log(log);
+	var usrtmp = "welcome " + socket.id.replace(/\/auth#/g, 'user ');
 	var co_msg = { 'msg' : usrtmp, 'tot' : connected };
 	io.of('/auth').emit('my-message', co_msg);
-	socket.on('user login', function (data) { checkData(data, socket) });
+	socket.on('user login', function (data) { checkcoData(data, socket) });
 	socket.on('disconnect', function() { connected -= 1; });
 });
+
+io
+.of('/register')
+.on('connection', function (socket) {
+	var log = socket.id.replace(/\/register#/g, 'User : ');
+	log += " connected to [/register] route";
+	console.log(log);
+	socket.on('user signin', function (data) { checkRegData(data, socket)});
+	socket.on('disconnect', function() { });
+})
