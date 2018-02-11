1da788734c626d909a81744c6c45ab887d6d7cbf
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-11 09:37:09 +0100

    adapt client socket handler to reflect changes in server side

--- coin_board/public/javascripts/auth_socket.js
+++ coin_board/public/javascripts/auth_socket.js
@@ -1,18 +1,50 @@
 $(document).ready(function() {
 	var auth = io.connect('http://localhost:3001/auth');
+	var clickDiv = document.getElementById("click");
+	$('#exampleModal').modal({
+		keyboard: false
+	})
+
+	function update_usr(data) {
+		/** create new div */
+		$.each(data, function (key, obj) {
+			console.log(key, obj)
+			if (data.errcode) {
+				var newline = $('<li class="text-danger">');
+				$('#dialogtext').text(' ');
+			} else {
+				var newline = $('<li class="text-primary">');
+			}
+			newline = newline.text(key + ' : ' + obj);
+			newline.appendTo('#dialogtext');
+		});
+		$('#exampleModal').modal('toggle');
+	}
+
+	function sendname() {
+		var inputDiv = document.getElementById("InputName");
+		var InputName = inputDiv.value.trim();
+		if (InputName.length > 2) {
+			auth.emit('user login', {logname: InputName});
+		}
+	}
 
 	auth.on('connection', function (socket) {
 		console.log('succesfully connected to auth socket');
 	});
 	auth.on('my-message', function (data) {
-		console.log(data.msg ? data.msg : data);
-	});
-	auth.on('error-message', function (data) {console.log(data.msg);});
-	$('#click').click(function() {
-		var InputName = $('#InputName').val().trim();
-		if (InputName.length > 2) {
-			console.log('click [' + InputName + '] ');
-			auth.emit('user login', {logname: InputName});
+		if (!data.msg) {
+			console.log(data);
+			update_usr(data);
+		} else {
+			console.log(data.msg);
+			update_usr(data);
 		}
-	})
+	});
+	auth.on('error-message', function (data) {
+		console.log(data.msg);
+		update_usr(data);
+	});
+	/** dom manip -send */
+	clickDiv.addEventListener("click", sendname);
 });
