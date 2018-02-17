0bfce76dd046793f5eac315fc242d3fa28c7c834
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-17 11:56:42 +0100

    clean test code and add header

--- tests/login_route-test.js
+++ tests/login_route-test.js
@@ -1,4 +1,8 @@
-var assert= require('assert');
+/**
+* @file Unit test file for login route functions
+* @author base on Express app and edited by Trevis Gulby
+*/
+
 var request = require('supertest');
 var test_url = '/login';
 var app = require('../coin_board/app');
@@ -6,46 +10,46 @@ var app = require('../coin_board/app');
 describe('Simple GET /login on app', function() {
 	it('respond with json', function(done) {
 		request(app)
-		.get(test_url)
-		.set('Accept', 'application/json')
-		.expect(200)
-		.end(function(err, res) {
-			if (err) return done(err);
-			done();
-		});
+			.get(test_url)
+			.set('Accept', 'application/json')
+			.expect(200)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
 	});
 	it('respond with 404', function(done) {
 		request(app)
-		.get(test_url + '/foobar')
-		.set('Accept', 'application/json')
-		.expect(404)
-		.end(function(err, res) {
-			if (err) return done(err);
-			done();
-		})
+			.get(test_url + '/foobar')
+			.set('Accept', 'application/json')
+			.expect(404)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
 	});
 });
 
 describe('Simple POST /login on app', function() {
 	it('respond 403 for now', function(done) {
 		request(app)
-		.post(test_url)
-		.set('Content-type', 'application/json')
-		.send('{"email":"foobar@bizz.com"}')
-		.expect(403)
-		.end(function(err, res) {
-			if (err) return done(err);
-			done();
-		});
+			.post(test_url)
+			.set('Content-type', 'application/json')
+			.send('{"email":"foobar@bizz.com"}')
+			.expect(403)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
 	});
 	it('respond with 404', function(done) {
 		request(app)
-		.get('/noexist')
-		.set('Accept', 'application/json')
-		.expect(404)
-		.end(function(err, res) {
-			if (err) return done(err);
-			done();
-		})
+			.get('/noexist')
+			.set('Accept', 'application/json')
+			.expect(404)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
 	});
 });
--- tests/myassets_route-test.js
+++ tests/myassets_route-test.js
@@ -1,4 +1,8 @@
-var assert= require('assert');
+/**
+* @file Unit test file for my assets route functions
+* @author base on Express app and edited by Trevis Gulby
+*/
+
 var request = require('supertest');
 var test_url = '/myassets';
 var app = require('../coin_board/app');
@@ -6,22 +10,22 @@ var app = require('../coin_board/app');
 describe('Simple GET / on app', function() {
 	it('respond with json', function(done) {
 		request(app)
-		.get(test_url)
-		.set('Accept', 'application/json')
-		.expect(200)
-		.end(function(err, res) {
-			if (err) return done(err);
-			done();
-		});
+			.get(test_url)
+			.set('Accept', 'application/json')
+			.expect(200)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
 	});
 	it('respond with 404', function(done) {
 		request(app)
-		.get(test_url + '/foobar')
-		.set('Accept', 'application/json')
-		.expect(404)
-		.end(function(err, res) {
-			if (err) return done(err);
-			done();
-		})
-	})
+			.get(test_url + '/foobar')
+			.set('Accept', 'application/json')
+			.expect(404)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 });
