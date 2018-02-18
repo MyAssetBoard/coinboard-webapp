04180c6ce5064e47eb30068d0d5fdaad2d035649
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-18 17:24:47 +0100

    add a begining of an auth method via session cookie and aes encrypt guid

--- coin_board/routes/login.js
+++ coin_board/routes/login.js
@@ -17,10 +17,18 @@ function render403(req, res) {
 	response += '\t& go rtfm :)\n';
 	res.status(403).send(response);
 }
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res) {
 	'use strict';
+	console.log(req.session);
 	res.render('login', param);
+});
+
+router.get('/id/:uid', function(req, res) {
+	console.log('received id request with value :');
+	console.log(req.params);
+	req.session.uid = req.params.uid;
+	res.redirect('http://localhost:3000/login');
 });
 
 router.post('/', render403);
