75f90c94afd4fefbd056324b890d6d34f3b02249
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-18 09:50:57 +0100

    add user id checking on get request , will provide two different views

--- coin_board/routes/myassets.js
+++ coin_board/routes/myassets.js
@@ -5,15 +5,59 @@
 
 var express;
 var router;
-var params;
+var crudMod;
+var defparams;
+var userid;
+var ObjectId;
 
 express = require('express');
 router = express.Router();
-params = require('../params/myassets_param');
+ObjectId = require('mongodb').ObjectId;
+defparams = require('../params/myassets_param');
+crudMod = require('../methods/mongo_crud');
+
+
+function checkUid(data) {
+	return new Promise((resolve, reject) => {
+		var crud = new crudMod('test2');
+		var val = new ObjectId(data.id);
+		console.log(val);
+		crud.FindInCollection('r_users', {_id: val }, function (res) {
+			if (res) {
+				resolve(res);
+			}
+			reject(new Error('Bad Id'));
+		});
+	});
+}
+
+function checkGetparam(req) {
+	if (req.query.id && req.query.id.length > 5) {
+		checkUid(req.query)
+			.then(function (res) {
+				console.log(res);
+				return true;
+			})
+			.catch(function (rej, err) {
+				console.error(rej.message);
+				if (err) throw err;
+				return false;
+			});
+	}
+	return false;
+}
 
 /* GET assets page. */
 router.get('/', function(req, res) {
-	res.render('assets', params);
+	userid = req.query.id;
+	if (userid) {
+		console.log('myassets route : received ' + userid + ' param');
+		checkGetparam(req);
+		defparams['id'] = userid;
+		res.render('assets', defparams);
+	} else {
+		res.render('assets', defparams);
+	}
 });
 
 module.exports = router;
