685bf539c0ed4d0f2783c35b5ca7d82519b089b7
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-23 12:11:35 +0100

    add istanbul parser coverage options

--- coin_board/app.js
+++ coin_board/app.js
@@ -29,6 +29,7 @@ var options = {
 var log = 'app.js| http options\n';
 log += '==== opts = [ ';
 log += JSON.stringify(options) + ' ]';
+/* istanbul ignore next */
 process.env.NODE_ENV == 'infosec' ? console.log(log) : log;
 
 var favOptions = {
@@ -59,9 +60,13 @@ app.use(express.static(path.join(__dirname, 'public/javascripts')));
 // Remove console log in production mode
 var output_avert = 'NODE_ENV=production| (No more console.log output)';
 output_avert += ' (unless true)';
+
+/* istanbul ignore if */
 if (process.env.NODE_ENV == 'production') {
 	console.log(output_avert);
 }
+
+/* istanbul ignore if */
 if (process.env.NODE_ENV == 'development') {
 	var output = 'NODE_ENV=development| (Use Morgan for logging requests)';
 	console.log(output);
@@ -86,24 +91,25 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
 	/** append header param to response */
 	for (var k in options) {
+		/* istanbul ignore if */
 		if (options.hasOwnProperty(k)) {
 			res.append(k, options[k]);
 		}
 	}
 	/** set locals, only providing error in development */
 	res.locals.message = err.message;
+	/* istanbul ignore next */
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 	/** render the error page */
 	res.status(err.status || 500);
 	/** Check if method is GET or POST only */
 	if (!allowedMethods.includes(req.method)) {
-		res.status(500).send('=> Not allowed ;)\n');
+		res.status(405).send('=> Not allowed ;)\n');
 	} else {
 		res.render('error', error_param);
 	}
-	if (next) {
-		next = {};
-	}
+	/* istanbul ignore if */
+	if (next) { next = {}; }
 });
 
 module.exports = app;
