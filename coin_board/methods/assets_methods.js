9e2fb42ac4f278aebed196c5af59d557c96e9127
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-23 12:14:39 +0100

    add better param checking methods following upgrades in test ;)

--- coin_board/methods/assets_methods.js
+++ coin_board/methods/assets_methods.js
@@ -3,33 +3,49 @@ const crudMod = require('../methods/mongo_crud');
 const authMod = require('../methods/auth_methods');
 var auth = new authMod();
 
-function Assets() {
-
+function isEncoded(str) {
+	try {
+		var res = decodeURIComponent(str);
+	} catch (e) {
+		if (e || !res) {return false;}
+	}
+	return true;
 }
 
-function addAssets(a) {
+function Assets(socket, io) {
+	this.socket ? socket : [];
+	this.io ? io : [];
+}
+
+Assets.prototype.addAssets = function (a) {
 	return new Promise((resolve, reject) => {
 		a.ticker = a.ticker.replace(/\W/g, '');
-		a.qtt = a.qtt.replace('(\\d)+\\.(\\d+)', '');
-		a.qtt = parseFloat(a.qtt);
-		a.id = decodeURIComponent(a.id);
-		a.id = auth.decryptUid(a.id);
+		a.qtt = typeof a.qtt === 'string' ?
+			a.qtt.replace('(\\d)+\\.(\\d+)', '') : a.qtt;
+		a.qtt = a.qtt ? parseFloat(a.qtt) : 0.00;
+		a.id = isEncoded(a.id) ? decodeURIComponent(a.id) : null;
+		a.id = a.id ? auth.decryptUid(a.id) : null;
 
-		var data = {
-			'symbol': a.ticker,
-			'qtt' : a.qtt
-		};
-		var crud = new crudMod('test2', 'r_users');
-		crud.InsertInField(a.id, 'assets', data, function(result) {
-			if (result) {resolve(result);}
-			reject(new Error('Db Error'));
-		});
+		var data = { 'symbol': a.ticker, 'qtt' : a.qtt };
+		if (a.id && a.qtt) {
+			var crud = new crudMod('test2', 'r_users');
+			crud.InsertInField(a.id, 'assets', data, function(result) {
+				if (result) {
+					resolve(result);
+				}
+				reject(new Error('Db Error'));
+			});
+		} else if (!a.qtt) {
+			reject(new Error('Invalid quantity'));
+		} else {
+			reject(new Error('Wrong request'));
+		}
 	});
-}
+};
 
 Assets.prototype.checkAssetData = function (data, socket, io) {
 	if (data['ticker'] && data['qtt'] && data['id']) {
-		addAssets(data)
+		Assets.addAssets(data)
 			.then(function (res) {
 				console.log(res);
 				io.of('/assets')
