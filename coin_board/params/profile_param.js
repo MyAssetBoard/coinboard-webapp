f6a753a3c7c794690f0d2f98565d6b20dc6797c4
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-24 23:43:15 +0100

    update view param remove copy paste options from model

--- coin_board/params/profile_param.js
+++ coin_board/params/profile_param.js
@@ -3,18 +3,10 @@
 * @author Trevis Gulby
 */
 
-var cdn = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js';
-var cdn1 = 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js';
-
 const param = {
 	title	: 'Profile',
 	author	: '© Copyright 2018 coin_board ;)',
 	page	: 'login',
-	scripts	: {
-		socketio	: cdn,
-		cryptojs	: cdn1,
-		mysocket	: 'auth_socket.js'
-	},
 	blocks : {
 		jumbo		: 'blocks/all/my_jumbotron',
 		topblock	: 'blocks/profile/my_profile'
