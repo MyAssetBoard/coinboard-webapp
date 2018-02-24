c10558826f6fb380f54007f371b8cb0c62d140e5
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-24 18:11:31 +0100

    add test for new profile route

---
+++ test/routes/profile_route-test.js
@@ -0,0 +1,38 @@
+/**
+* @file Unit test file for index route functions
+* @author base on Express app and edited by Trevis Gulby
+*/
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var request = require('supertest');
+var test_url = '/profile';
+var app = require('../../coin_board/app');
+
+describe('GET profile', function() {
+	before(function() {
+		return this.spy = sinon.spy(app, 'render');
+	});
+	after(function() {
+		return this.spy.restore();
+	});
+	it('should exist', function() {
+		return request(app).get(test_url).expect(200);
+	});
+	return it('should render the "index" view', function() {
+		return expect(this.spy.getCall(0).args[0]).to.be.eql('page');
+	});
+});
+
+describe('GET /unknow', function ()  {
+	it('it respond with 404', function(done) {
+		request(app)
+			.get(test_url + '/noextsgsg')
+			.set('Accept', 'application/json')
+			.expect(404)
+			.end(function(err) {
+				if (err) return done(err);
+				done();
+			});
+	});
+});
