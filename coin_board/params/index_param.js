44306dce9245e83b86154f1192295ff142081802
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-26 10:04:57 +0100

    starting to rewrite index page

--- coin_board/params/index_param.js
+++ coin_board/params/index_param.js
@@ -3,19 +3,12 @@
 * @author Trevis Gulby
 */
 
-var cdn = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js';
 const param = {
 	title	: 'Coin_Board',
 	author	: '© Copyright 2018 coin_board',
 	page	: 'index',
-	scripts	: {
-		socket	: cdn,
-		trade 	: 'tradestream.js'
-	},
 	blocks : {
-		jumbo		: 'blocks/all/my_jumbotron',
-		ticker		: 'blocks/index/ticker_panel',
-		livestream	: 'blocks/index/tradestream'
+		jumbo		: 'blocks/all/my_jumbotron'
 	}
 };
 
