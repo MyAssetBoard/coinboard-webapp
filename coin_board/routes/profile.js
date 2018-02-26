fe7f2bb8d3d0a4e01e5b54447b91579c1257afad
Trevis Gulby <trevisgulby@protonmail.ch>
2018-02-26 10:05:31 +0100

    minor fix on copy paste typo

--- coin_board/routes/profile.js
+++ coin_board/routes/profile.js
@@ -12,19 +12,19 @@ router = express.Router();
 authMod = require('../methods/auth_methods');
 const param = require('../params/profile_param');
 
-/* GET home page. */
+/* GET profile page. */
 router.get('/', function(req, res, next) {
 	var chck = req.cookies;
 
 	if (chck && chck.uid) {
-		var log = '/INDEX-route| Auth user, session below\n[';
+		var log = '/PROFIlE-route| Auth user, session below\n[';
 		log += JSON.stringify(chck) + ']';
 		process.env.NODE_ENV == 'development' ? console.log(log) : log;
 		var auth = new authMod();
 		auth.userisAuth(chck.uid, 'profile')
 			.then(function(result) {
 				var dup = param;
-				var log = 'index| push user info in params \n[';
+				var log = 'profile| push user info in params \n[';
 				res.locals.data = result;
 				/* istanbul ignore next */
 				log += JSON.stringify(res.locals.data) + ']';
@@ -38,7 +38,7 @@ router.get('/', function(req, res, next) {
 				next(err);
 			});
 	} else {
-		log = 'index-route| NonAUth user, session below\n[';
+		log = 'profile-route| NonAUth user, session below\n[';
 		log += JSON.stringify(chck) + '] cookie ? [';
 		log += JSON.stringify(req.cookies) + ']';
 		/* istanbul ignore next */
