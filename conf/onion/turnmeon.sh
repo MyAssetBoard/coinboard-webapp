b97bc7bdd1bc7411b31ce674e14760fe5f6bfc1e
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-01 18:28:35 +0100

    add adequate docker setup helper

---
+++ conf/onion/turnmeon.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#    ______                                      ____
+#   /_  __/_  ___________     ____ ___  ___     / __ \____
+#    / / / / / / ___/ __ \   / __ `__ \/ _ \   / / / / __ \
+#   / / / /_/ / /  / / / /  / / / / / /  __/  / /_/ / / / /
+#  /_/  \__,_/_/  /_/ /_/  /_/ /_/ /_/\___/   \____/_/ /_/
+# ---------------------TurnMeOn.sh --------------------
+# Usage : launched by the Dockerfile $ENTRYPOINT for Onion docker service
+chown -R root /var/lib/tor
+chmod -R 700  /var/lib/tor
+tor && service nginx start && ./INIT_DEV.sh -rl
+screen -dmS applog ./node_modules/pm2/bin/pm2 logs
+screen -dmS servlog tail -f /var/log/nginx/*.log && exec "$@"
