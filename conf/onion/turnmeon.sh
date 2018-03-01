39e3ba018bc42a7b0da8715d9caf176739beec7c
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-01 22:11:24 +0100

    adapt init script as node env param req

--- conf/onion/turnmeon.sh
+++ conf/onion/turnmeon.sh
@@ -8,6 +8,7 @@
 # Usage : launched by the Dockerfile $ENTRYPOINT for Onion docker service
 chown -R root /var/lib/tor
 chmod -R 700  /var/lib/tor
+export NODE_ENV='production' && export SERV_ENV='onion'
 tor && service nginx start && ./INIT_DEV.sh -rl
 screen -dmS applog ./node_modules/pm2/bin/pm2 logs
 screen -dmS servlog tail -f /var/log/nginx/*.log && exec "$@"
