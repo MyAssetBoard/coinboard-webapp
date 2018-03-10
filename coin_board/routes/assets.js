3b999dca8f642950d65aae2ca747fec9cf906e1f
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-11 00:21:30 +0100

    add brand new DataJunk function for assets page!(need unit test)

--- coin_board/routes/assets.js
+++ coin_board/routes/assets.js
@@ -6,15 +6,125 @@
 const express = require( 'express' );
 const router = express.Router();
 const Auth = require( '../methods/auth_methods' );
+const DataJunk = require( '../bin/data_junk' );
 const auth = new Auth();
+const data = new DataJunk();
 const param = require( '../params/myassets_param' );
 
+const roads = {
+        a: {
+                path: '/assets/dashboard',
+                block: 'centerblock_dashboard',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_dashboard',
+                        };
+                        callback && callback( res );
+                },
+        },
+        b: {
+                path: '/assets/infofeed',
+                block: 'centerblock_infofeed',
+                func: function( callback ) {
+                        data.pukedata( {} ).then( function( r ) {
+                                let xx = {
+                                        block: 'centerblock_infofeed',
+                                        feed: r[0].feed,
+                                };
+                                callback && callback( xx );
+                        } );
+                },
+        },
+        c: {
+                path: '/assets/pricefeed',
+                block: 'centerblock_pricefeed',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_pricefeed',
+                        };
+                        callback && callback( res );
+                },
+        },
+        d: {
+                path: '/assets/trade',
+                block: 'centerblock_pricefeed',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_pricefeed',
+                        };
+                        callback && callback( res );
+                },
+        },
+        e: {
+                path: '/assets/api/param',
+                block: 'centerblock_pricefeed',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_pricefeed',
+                        };
+                        callback && callback( res );
+                },
+        },
+        f: {
+                path: '/assets/trade/reports/currm',
+                block: 'centerblock_pricefeed',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_pricefeed',
+                        };
+                        callback && callback( res );
+                },
+        },
+        g: {
+                path: '/assets/trade/reports/currquart',
+                block: 'centerblock_pricefeed',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_pricefeed',
+                        };
+                        callback && callback( res );
+                },
+        },
+        h: {
+                path: '/assets/trade/reports/crowddata',
+                block: 'centerblock_pricefeed',
+                func: function( callback ) {
+                        let xx = {
+                                block: 'centerblock_pricefeed',
+                        };
+                        callback && callback( res );
+                },
+        },
+};
+
+function setpagecontent( req ) {
+        return new Promise( ( resolve, reject ) => {
+                let res = {};
+                for ( let property in roads ) {
+                        if ( roads[property].path == req.originalUrl ) {
+                                if ( roads[property].func ) {
+                                        res.fn = roads[property].func;
+                                }
+                        }
+                }
+                if ( res.fn ) {
+                        res.fn( function( r ) {
+                                if ( r ) {
+                                        resolve( r );
+                                }
+                        } );
+                } else {
+                        resolve( 'nop' );
+                }
+        } );
+}
+
 /* GET assets page. */
-router.get( '/', function( req, res, next ) {
+router.get( '/*', function( req, res, next ) {
         let chck = req.cookies;
 
         if ( chck && chck.uid && auth.isvaliduid( chck.uid ) ) {
-                let log = '/MYASSETS-route : Auth user, session below\n[';
+                let log = req.originalUrl + '-route : Auth user, session below\n[';
                 log += JSON.stringify( chck ) + ']';
                 process.env.NODE_ENV == 'development'
                         ? console.log( log )
@@ -29,16 +139,28 @@ router.get( '/', function( req, res, next ) {
                         process.env.NODE_ENV == 'development'
                                 ? console.log( log )
                                 : log;
-                        res.render( 'page', dup );
-                } ).catch( function( err ) {
-                        if ( err ) {
-                                throw err;
-                        }
+                        setpagecontent( req ).then( function( d ) {
+                                if ( d != 'nop' ) {
+                                        res.locals.routes = {
+                                                a: d.block,
+                                        };
+                                        if ( d.feed ) {
+                                                res.locals.news = d.feed;
+                                        }
+                                }
+                                res.render( 'page', dup );
+                        } ).catch( function( rej, err ) {
+                                if ( err ) {
+                                        reject( err );
+                                        throw err;
+                                }
+                        } );
+                } ).catch( function( rej, err ) {
                         next( err );
                 } );
         } else {
                 /* istanbul ignore next */
-                log = 'myassets-route| NonAUth user, session below\n[';
+                log = req.originalUrl + '-route| NonAUth user, session below\n[';
                 log += JSON.stringify( chck ) + '] cookie ? [';
                 log += JSON.stringify( req.cookies ) + ']';
                 /* istanbul ignore next */
