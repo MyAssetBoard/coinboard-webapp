6de3c3b8a65371fac5562fc6c88624a6e5724b96
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-18 07:56:26 +0100

    add header, and error prevention with check clean before calling tostring

--- coin_board/bin/djunk/reqmodels.js
+++ coin_board/bin/djunk/reqmodels.js
@@ -1,6 +1,18 @@
-const ctafjson = '/v1/public/yql?q=select%20title%2Clink%2Cdescription%20from%20rss%20' + 'where%20url%3D%22https%3A%2F%2Fcointelegraph.com%2Frss%3Fformat%3Dxml%22' + '&format=json&diagnostics=true&callback=';
-const cryptonewsjson = '/v1/public/yql?q=select%20title%2Clink%2Cdescription%20from%20rss%20' + 'where%20url%3D%22https%3A%2F%2Fcryptocurrencynews.com%2Ffeed%2F%3Fformat%3Dxml%22' + '&format=json&diagnostics=true&callback=';
-let d = new Date();
+/**
+* @file Crawler feeds and pre-parsing methods for DajaJunk module
+* @author Trevis Gulby
+*/
+
+const d = new Date();
+let ctafjson = '/v1/public/yql?q=select%20title%2Clink%2Cdescription';
+ctafjson += '%20from%20rss%20' + 'where%20url';
+ctafjson += '%3D%22https%3A%2F%2Fcointelegraph.com%2Frss%3Fformat%3Dxml%22';
+ctafjson += '&format=json&diagnostics=true&callback=';
+let cryptonewsjson = '/v1/public/yql?q=select%20title%2Clink%2Cdescription';
+cryptonewsjson += '%20from%20rss%20' + 'where%20url';
+cryptonewsjson += '%3D%22https%3A%2F%2Fcryptocurrencynews.com%2F';
+cryptonewsjson += 'feed%2F%3Fformat%3Dxml%22';
+cryptonewsjson += '&format=json&diagnostics=true&callback=';
 let tmpst = d.getDate();
 tmpst += ( d.getMonth() + 1 ) < 10
         ? '0' + ( d.getMonth() + 1 )
@@ -10,6 +22,7 @@ tmpst += '_' + d.getHours() + '-';
 tmpst += d.getMinutes() < 10
         ? '0' + d.getMinutes() + '.json'
         : d.getMinutes() + '.json';
+
 const reqmodels = {
         ctaf: {
                 id: 'ctaf',
@@ -21,7 +34,9 @@ const reqmodels = {
                 fname: './DTAFOOD/news/ctaf-' + tmpst,
                 clean: function( mess ) {
                         let clean = mess.match( /<p>(.*?)<\/p>/g );
-                        clean = clean.toString().replace( /<\/?p>/g, '' );
+                        clean = !clean
+                                ? 'PARSING-ERROR'
+                                : clean.toString().replace( /<\/?p>/g, '' );
                         return clean;
                 },
         },
@@ -35,10 +50,13 @@ const reqmodels = {
                 fname: './DTAFOOD/news/crnews-' + tmpst,
                 clean: function( mess ) {
                         let rgx1 = /(<([^>]+)>)/ig;
-                        let rgx2 = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
-                        let result = mess.replace( rgx1, '' );
-                        result = result.replace( rgx2, ' ' );
-                        return result.toString().split( 'Read The Full' )[0];
+                        let rgx2 = /[`~@#%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+                        let rt = mess.replace( rgx1, '' );
+                        rt = rt.replace( rgx2, ' ' );
+                        rt = !rt || !rt.length
+                                ? 'PARSING-ERROR'
+                                : rt.toString().split( 'Read The Full' )[0];
+                        return rt;
                 },
         },
 };
