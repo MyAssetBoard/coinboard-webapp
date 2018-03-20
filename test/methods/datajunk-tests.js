f43c3b75e44b891c00566f6108e74d29c0c96084
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-20 08:30:19 +0100

    update test for datajunks methods

--- test/methods/datajunk-tests.js
+++ test/methods/datajunk-tests.js
@@ -4,21 +4,23 @@ const chaiAsPromised = require( 'chai-as-promised' );
 chai.use( chaiAsPromised );
 const DataJunk = require( '../../coin_board/methods/datajunk_methods' );
 const djunk = new DataJunk();
-let toeat = {
-        feed: [
-                {
-                        'title': 'Breaking: Trustee Of Infamous Mt. Gox Denies BTC, BCH Sales Affected Crypto Markets',
-                        'link': 'https://cointelegraph.com/news/breaking-trustee-of-infamous-mt-gox-denies-btc-bch-sales-affected-crypto-markets',
-                        'description': 'Mt. Gox trustee blamed for crypto market crash says all coins were sold without ‘affecting the market price’ #NEWS',
-                },
-        ],
-};
+let toeat = [
+        {
+                feed: [
+                        {
+                                'title': 'Breaking: Trustee Of Infamous Mt. Gox Denies BTC, BCH Sales Affected Crypto Markets',
+                                'link': 'https://cointelegraph.com/news/breaking-trustee-of-infamous-mt-gox-denies-btc-bch-sales-affected-crypto-markets',
+                                'description': 'Mt. Gox trustee blamed for crypto market crash says all coins were sold without ‘affecting the market price’ #NEWS',
+                        },
+                ],
+        },
+];
 let res = [
         {
-                title: 'Breaking: Trustee Of Infamous Mt. Gox Denies BTC, BCH Sales Affected Crypto Markets',
-                subject: 'btc,markets',
-                type: '',
-                flag: '',
+                'title': 'breaking: trustee of infamous mt. gox denies btc, bch sales affected crypto markets',
+                'subject': 'btc,markets,',
+                'type': 'bear',
+                'flag': 'sales',
         },
 ];
 
@@ -26,6 +28,7 @@ let desc = 'Test eat function for good parsing';
 describe( desc, function() {
         desc = 'It return an object with keys : title, subject, type, flag';
         it( desc, function() {
-                return assert.equal( djunk.eat( toeat ), res );
+                let tst = djunk.eat( toeat );
+                return assert.deepEqual( tst[0], res[0] );
         } );
 } );
