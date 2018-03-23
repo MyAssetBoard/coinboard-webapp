5643be54bff1c9881bfaebdb79f920bec62024a2
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-23 09:10:54 +0100

    update method to get onion address

--- coin_board/params/profile_param.js
+++ coin_board/params/profile_param.js
@@ -3,15 +3,31 @@
 * @author Trevis Gulby
 */
 
+/** Classic ip get method :*/
 const os = require( 'os' );
 const ni = os.networkInterfaces();
 const myip = ni.eth0[0].address;
+/** Onion addr get method :*/
+const toraddr = {
+        view: function() {
+                let buff = new Buffer( 22 );
+                let fn = '/var/lib/tor/hidnview/hostname';
+                buff = fs.readFileSync( fn, 'ascii' );
+                return buff.toString().replace( /\s+/g, ' ' ).trim();
+        },
+        socks: function() {
+                let buff = new Buffer( 22 );
+                let fn = '/var/lib/tor/hidnws/hostname';
+                buff = fs.readFileSync( fn, 'ascii' );
+                return buff.toString().replace( /\s+/g, ' ' ).trim();
+        },
+};
 
 const appvurl = process.env.SERV_ENV == 'onion'
-        ? 'http://xu6ylq4kzadh7bcm.onion/'
+        ? toraddr.view()
         : 'http://' + myip + ':3000/';
 const appsurl = process.env.SERV_ENV == 'onion'
-        ? 'http://q4s3a47gunuo5tie.onion:124/'
+        ? toraddr.socks()
         : 'http://' + myip + ':3001/';
 
 const param = {
--- coin_board/params/signin_param.js
+++ coin_board/params/signin_param.js
@@ -2,16 +2,31 @@
 * @file Signin view params
 * @author Trevis Gulby
 */
-
+/** Classic ip get method :*/
 const os = require( 'os' );
 const ni = os.networkInterfaces();
 const myip = ni.eth0[0].address;
+/** Onion addr get method :*/
+const toraddr = {
+        view: function() {
+                let buff = new Buffer( 22 );
+                let fn = '/var/lib/tor/hidnview/hostname';
+                buff = fs.readFileSync( fn, 'ascii' );
+                return buff.toString().replace( /\s+/g, ' ' ).trim();
+        },
+        socks: function() {
+                let buff = new Buffer( 22 );
+                let fn = '/var/lib/tor/hidnws/hostname';
+                buff = fs.readFileSync( fn, 'ascii' );
+                return buff.toString().replace( /\s+/g, ' ' ).trim();
+        },
+};
 
 const appvurl = process.env.SERV_ENV == 'onion'
-        ? 'http://xu6ylq4kzadh7bcm.onion/'
+        ? toraddr.view()
         : 'http://' + myip + ':3000/';
 const appsurl = process.env.SERV_ENV == 'onion'
-        ? 'http://q4s3a47gunuo5tie.onion:124/'
+        ? toraddr.socks()
         : 'http://' + myip + ':3001/';
 const cdn = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js';
 const cdn1 = 'https://cdn.jsdelivr.net/npm/web3@0.19.0/dist/web3.min.js';
