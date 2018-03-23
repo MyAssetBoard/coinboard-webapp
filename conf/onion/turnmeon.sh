02ee13c323f09f9d465e48460c116db35dcb5d77
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-23 09:43:14 +0100

    update listening address via script

--- conf/onion/turnmeon.sh
+++ conf/onion/turnmeon.sh
@@ -7,7 +7,12 @@
 # ---------------------TurnMeOn.sh --------------------
 # Usage : launched by the Dockerfile $ENTRYPOINT for Onion docker service
 sudo chown -R fofo:fofo /var/lib/tor && \
+sudo chmod -R 700 /var/lib/tor && \
 sudo chown -R fofo:fofo /var/log/tor && \
-sudo chown -R fofo:fofo log tmpdata && \
-tor && sudo nginx && ./INIT_DEV.sh -rl production onion && \
+sudo chown -R fofo:fofo log tmpdata && tor \
+&& echo "proxy_pass $(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3000;" \
+> /usr/src/app/ipview && \
+echo "proxy_pass $(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3001;" \
+> /usr/src/app/ipsocks && \
+ sudo nginx && ./INIT_DEV.sh -rl production onion && \
 screen -dmS applog ./node_modules/pm2/bin/pm2 logs
