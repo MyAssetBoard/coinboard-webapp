b4649e00d06669eff31692c403003196e1e889db
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-23 09:46:28 +0100

    correcting ipaddr script

--- conf/onion/turnmeon.sh
+++ conf/onion/turnmeon.sh
@@ -10,9 +10,9 @@ sudo chown -R fofo:fofo /var/lib/tor && \
 sudo chmod -R 700 /var/lib/tor && \
 sudo chown -R fofo:fofo /var/log/tor && \
 sudo chown -R fofo:fofo log tmpdata && tor \
-&& echo "proxy_pass $(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3000;" \
+&& echo "proxy_pass http://$(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3000;" \
 > /usr/src/app/ipview && \
-echo "proxy_pass $(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3001;" \
+echo "proxy_pass http://$(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3001;" \
 > /usr/src/app/ipsocks && \
  sudo nginx && ./INIT_DEV.sh -rl production onion && \
 screen -dmS applog ./node_modules/pm2/bin/pm2 logs
