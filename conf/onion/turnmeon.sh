8071b9b2385553ad9793db20b2f0da2b933c0d41
Trevis Gulby <trevisgulby@protonmail.ch>
2018-03-23 13:43:10 +0100

    correcting typo in init script

--- conf/onion/turnmeon.sh
+++ conf/onion/turnmeon.sh
@@ -10,13 +10,14 @@ cd /usr/src/app && \
 sudo chown -R fofo:fofo /var/lib/tor && \
 sudo chmod -R 700 /var/lib/tor && \
 sudo chown -R fofo:fofo /var/log/tor && \
-sudo chown -R fofo:fofo log tmpdata && tor \
+sudo chown -R fofo:fofo log tmpdata && \
 sudo chown -R fofo /var/log/nginx && \
+tor && \
 export IP=$(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1) \
 && echo "proxy_pass http://$(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3000;" \
 > /usr/src/app/ipview && \
 echo "proxy_pass http://$(ip -o -4 a s eth0 | awk '{ print $4 }' | cut -d/ -f1):3001;" \
 > /usr/src/app/ipsock && \
-mkdir -p /usr/src/app/DTAFOOD/news && mkdir /usr/src/app/DTAFOOD/price \
+mkdir -p /usr/src/app/DTAFOOD/news && mkdir /usr/src/app/DTAFOOD/price && \
 sudo nginx && ./INIT_DEV.sh -rl production onion && \
 screen -dmS applog ./node_modules/pm2/bin/pm2 logs
