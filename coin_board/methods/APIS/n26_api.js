0353f114dad6314bb2631bdfc1ab4fada12dbc4a
Trevis Gulby <trevisgulby@protonmail.ch>
2018-04-09 12:45:32 +0200

    clean N26 module method code

--- coin_board/methods/APIS/n26_api.js
+++ coin_board/methods/APIS/n26_api.js
@@ -1,55 +1,58 @@
 /**
-* @file N26 APIS requests definitions
-* @author N26 node module and Trevis Gulby
-*/
-
-const N26 = require( 'n26' );
+ * @file N26 APIS requests definitions
+ * @author N26 node module and Trevis Gulby
+ */
 
 /**
-* @param {Object} id
-* @param {string} key
-* @return {Object} res
-*/
-function gettrstats( id, key ) {
-        return new Promise( ( resolve, reject ) => {
-                const acc = new N26( id.usr, id.pw );
-                let res = {
-                        cred: key + ' credit : ',
-                        char: key + ' charges : ',
-                        tot: key + ' cost : ',
-                };
-                acc.then( function( account ) {
-                        account.transactions( {text: key} ).then( ( tr ) => {
-                                let sum = 0;
-                                let min = 0;
-                                let plus = 0;
-                                for ( el in tr ) {
-                                        if ( tr[el].amount ) {
-                                                let y = tr[el].amount;
-                                                min += y <= 0
-                                                        ? y
-                                                        : 0;
-                                                plus += y >= 0
-                                                        ? y
-                                                        : 0;
-                                                sum += y;
-                                        }
-                                }
-                                res.cred += plus + ' €';
-                                res.char += min + ' €';
-                                res.tot += sum + ' €';
-                                resolve( res );
-                        } );
-                } );
-        } );
+ * My N26api overload
+ * @class
+ */
+class n26Api {
+    /** @constructor */
+    constructor() {
+        /** Perfect module for perfect usage */
+        this.N26 = require('n26');
+    }
 }
-
-if ( process.env.LAUNCH_TASK == 'ask' ) {
-        let id = {
-                usr: '',
-                pw: '',
+/** Give expense, credit and sold for a key elem
+ * @param {Object} id usr + psswd combo
+ * @param {string} key the element to sum about
+ * @return {Promise} res cred, deb and sum on string format if resolved
+ */
+n26Api.prototype.gettrstats = function(id, key) {
+    let _this = this;
+    return new Promise((resolve, reject) => {
+        const acc = new _this.N26(id.usr, id.pw);
+        let res = {
+            cred: key + ' credit : ',
+            char: key + ' charges : ',
+            tot: key + ' cost : ',
         };
-        gettrstats( id, 'coinbase' ).then( function( res ) {
-                console.log( res );
-        } );
-}
+        acc.then(function(account) {
+            account.transactions(
+            {
+                text: key,
+            }).then((tr) => {
+                let sum = 0;
+                let min = 0;
+                let plus = 0;
+                for (el in tr) {
+                    if (tr[el].amount) {
+                        let y = tr[el].amount;
+                        min += y <= 0 ?
+                            y :
+                            0;
+                        plus += y >= 0 ?
+                            y :
+                            0;
+                        sum += y;
+                    }
+                }
+                res.cred += plus + ' €';
+                res.char += min + ' €';
+                res.tot += sum + ' €';
+                resolve(res);
+            });
+        });
+    });
+};
