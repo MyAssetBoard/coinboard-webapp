26be0b35031e82afcc04b1aa71dd62c69f5abf25
Trevis Gulby <trevisgulby@protonmail.ch>
2018-04-12 11:06:20 +0200

    add new Apis module dependency for /assets/api/param request

--- coin_board/routes/assets/assets_roads.js
+++ coin_board/routes/assets/assets_roads.js
@@ -1,93 +1,139 @@
 /**
-* @file Assets routes params and paths definitions (use datajunk module)
-* @author Trevis Gulby
-*/
+ * @file Assets routes params and paths definitions (use datajunk module)
+ * @author Trevis Gulby
+ */
 
-const DataJunk = require( '../../methods/datajunk_methods' );
+const Apis = require('../../methods/api_methods');
+const api = new Apis();
+const DataJunk = require('../../methods/datajunk_methods');
 const data = new DataJunk();
 const roads = {
-        a: {
-                path: '/assets/dashboard',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['cb_db'],
-                        };
-                        callback && callback( xx );
-                },
+    a:
+    {
+        path: '/assets/dashboard',
+        getd: function(callback) {
+            let xx = {
+                blocks: ['cb_db'],
+                scripts: ['/assets_management.js'],
+            };
+            callback && callback(xx);
         },
-        b: {
-                path: '/assets/infofeed',
-                getd: function( callback ) {
-                        data.pukedata( {} ).then( function( res ) {
-                                let mine = data.eat( res );
-                                let rt = {};
-                                let fd = [];
-                                for ( el in res ) {
-                                        if ( res[el].feed ) {
-                                                fd = fd.concat( res[el].feed );
-                                        }
-                                }
-                                rt['blocks'] = ['cb_ifeed', 'cb_dmfeed.ejs'];
-                                rt['feed'] = fd;
-                                rt['dm'] = mine;
-                                callback && callback( rt );
-                        } );
-                },
+        stripd: function(d) {
+            if (d.hasOwnProperty('apisv2')) {
+                let todel = 'apisv2';
+                delete d[todel];
+            }
         },
-        c: {
-                path: '/assets/pricefeed',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['cb_pfeed'],
-                        };
-                        callback && callback( xx );
-                },
+    },
+    b:
+    {
+        path: '/assets/infofeed',
+        getd: function(callback) {
+            data.pukedata(
+            {}).then(function(res) {
+                let mine = data.eat(res);
+                let rt = {};
+                let fd = [];
+                for (el in res) {
+                    if (res[el].feed) {
+                        fd = fd.concat(res[el].feed);
+                    }
+                }
+                rt['blocks'] = ['cb_ifeed', 'cb_dmfeed.ejs'];
+                rt['feed'] = fd;
+                rt['dm'] = mine;
+                callback && callback(rt);
+            });
         },
-        d: {
-                path: '/assets/trade',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['cb_pfeed'],
-                        };
-                        callback && callback( xx );
-                },
+        stripd: function(d) {
+            if (d.hasOwnProperty('apisv2')) {
+                let todel = 'apisv2';
+                delete d[todel];
+            }
+            if (d.hasOwnProperty('assets')) {
+                let todel = 'assets';
+                delete d[todel];
+            }
         },
-        e: {
-                path: '/assets/api/param',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['api_param'],
-                        };
-                        callback && callback( xx );
-                },
+    },
+    c:
+    {
+        path: '/assets/pricefeed',
+        getd: function(callback) {
+            let xx = {
+                blocks: ['cb_pfeed'],
+            };
+            callback && callback(xx);
         },
-        f: {
-                path: '/assets/trade/reports/currm',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['cb_pfeed'],
-                        };
-                        callback && callback( xx );
-                },
+        stripd: function(d) {
+            if (d.hasOwnProperty('apisv2')) {
+                let todel = 'apisv2';
+                delete d[todel];
+            }
         },
-        g: {
-                path: '/assets/trade/reports/currquart',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['cb_pfeed'],
-                        };
-                        callback && callback( xx );
-                },
+    },
+    d:
+    {
+        path: '/assets/trade',
+        getd: function(callback) {
+            let xx = {
+                blocks: ['cb_pfeed'],
+            };
+            callback && callback(xx);
         },
-        h: {
-                path: '/assets/trade/reports/crowddata',
-                getd: function( callback ) {
-                        let xx = {
-                                blocks: ['cb_pfeed'],
-                        };
-                        callback && callback( xx );
-                },
+        stripd: function(d) {},
+    },
+    e:
+    {
+        path: '/assets/api/param',
+        getd: function(callback) {
+            api.getAccounts(
+            {}).then(function(res, err) {
+                let xx = {
+                    blocks: ['api_param'],
+                    scripts: ['/api_settings.js'],
+                    userapi: res,
+                };
+                callback && callback(xx);
+            });
         },
+        stripd: function(d) {
+            if (d.hasOwnProperty('assets')) {
+                let todel = 'assets';
+                delete d[todel];
+            }
+        },
+    },
+    f:
+    {
+        path: '/assets/trade/reports/currm',
+        getd: function(callback) {
+            let xx = {
+                blocks: ['cb_pfeed'],
+            };
+            callback && callback(xx);
+        },
+    },
+    g:
+    {
+        path: '/assets/trade/reports/currquart',
+        getd: function(callback) {
+            let xx = {
+                blocks: ['cb_pfeed'],
+            };
+            callback && callback(xx);
+        },
+    },
+    h:
+    {
+        path: '/assets/trade/reports/crowddata',
+        getd: function(callback) {
+            let xx = {
+                blocks: ['cb_pfeed'],
+            };
+            callback && callback(xx);
+        },
+    },
 };
 
 module.exports = roads;
