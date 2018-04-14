e089f737a7c2f417c62f6aa66e0e084d8c950128
Trevis Gulby <trevisgulby@protonmail.ch>
2018-04-14 17:49:19 +0200

    Refactoring task again, crud module is better but still not perfect still wip

--- coin_board/controllers/mongo_crud.js
+++ coin_board/controllers/mongo_crud.js
@@ -13,12 +13,8 @@ class Crud {
      * r_users by default
      */
     constructor(dbName, coll) {
-        this.dbName = dbName ?
-            dbName :
-            'test2';
-        this.coll = coll ?
-            coll :
-            'r_users';
+        this.dbName = dbName ? dbName : 'test2';
+        this.coll = coll ? coll : 'r_users';
         /** Mongodb client object import */
         this.MongoClient = require('mongodb').MongoClient;
         /** Mongodb object id object import */
@@ -35,32 +31,20 @@ class Crud {
  */
 Crud.prototype.insert = function(coll, data, callback) {
     let _this = this;
-    _this.MongoClient.connect(this.uri).then(function(db) {
+    _this.MongoClient.connect(this.uri).then((db) => {
         const dbo = db.db(_this.dbName);
-        let log = 'MONGO - Connected to ' + _this.dbName;
-        process.env.NODE_ENV == 'development' ?
-            console.log(log) :
-            log;
-        dbo.collection(coll).insertOne(data).then(function(res) {
-            log = 'MONGO| 1 document inserted :\n[';
-            log += JSON.stringify(data) + ']';
-            process.env.NODE_ENV == 'devdb' ?
-                console.log(log) :
-                log;
+        let log = 'Insert| MONGO - Connected to ' + _this.dbName;
+        process.env.NODE_ENV == 'development' ? console.log(log) : log;
+        dbo.collection(coll).insertOne(data).then((res) => {
             db.close();
-            callback && callback(res);
-            return res;
-        }).catch(function(rej, err) {
-            if (err || rej) {
-                callback && callback(err);
-                throw err;
-            }
+            log = 'MONGO - Inserted :\n[' + JSON.stringify(data) + ']';
+            process.env.NODE_ENV == 'development' ? console.log(log) : log;
+            return callback && callback(res);
+        }).catch((err) => {
+            return callback && callback(err);
         });
-    }).catch(function(rej, err) {
-        if (err || rej) {
-            callback && callback(err);
-            throw err;
-        }
+    }).catch(function(err) {
+        return callback && callback(err);
     });
 };
 
@@ -71,25 +55,18 @@ Crud.prototype.insert = function(coll, data, callback) {
 Crud.prototype.checkcred = function(c, callback) {
     let _this = this;
     if (c.username && c.socketid) {
-        _this.MongoClient.connect(this.uri).then(function(db) {
-            let log = 'MONGO - Connected to ' + _this.dbName;
-            process.env.NODE_ENV == 'development' ?
-                console.log(log) :
-                log;
+        _this.MongoClient.connect(this.uri).then((db) => {
+            let log = 'Checkcred| MONGO - Connected to ' + _this.dbName;
+            process.env.NODE_ENV == 'development' ? console.log(log) : log;
             let dbo = db.db(_this.dbName);
-            dbo.collection(_this.coll).findOne(c).then(function(res) {
+            dbo.collection(_this.coll).findOne(c).then((res) => {
                 db.close();
-                callback && callback(res);
-                return res;
-            }).catch(function(err) {
-                if (err) {
-                    throw err;
-                }
+                return callback && callback(res);
+            }).catch((err) => {
+                return callback && callback(err);
             });
-        }).catch(function(err) {
-            if (err) {
-                throw err;
-            }
+        }).catch((err) => {
+            return callback && callback(err);
         });
     }
 };
@@ -100,25 +77,18 @@ Crud.prototype.checkcred = function(c, callback) {
  */
 Crud.prototype.finduser = function(who, callback) {
     let _this = this;
-    _this.MongoClient.connect(this.uri).then(function(db) {
-        let log = 'MONGO - Connected to ' + _this.dbName;
-        process.env.NODE_ENV == 'development' ?
-            console.log(log) :
-            log;
+    _this.MongoClient.connect(this.uri).then((db) => {
+        let log = 'Finduser| MONGO - Connected to ' + _this.dbName;
+        process.env.NODE_ENV == 'development' ? console.log(log) : log;
         let dbo = db.db(_this.dbName);
-        dbo.collection(_this.coll).findOne(who).then(function(res) {
+        dbo.collection(_this.coll).findOne(who).then((res) => {
             db.close();
-            callback && callback(res);
-            return res;
-        }).catch(function(err) {
-            if (err) {
-                throw err;
-            }
+            return callback && callback(res);
+        }).catch((err) => {
+            return callback && callback(err);
         });
-    }).catch(function(err) {
-        if (err) {
-            throw err;
-        }
+    }).catch((err) => {
+        return callback && callback(err);
     });
 };
 
@@ -128,26 +98,20 @@ Crud.prototype.finduser = function(who, callback) {
  */
 Crud.prototype.find = function(what, callback) {
     let _this = this;
-    _this.MongoClient.connect(this.uri).then(function(db) {
-        let log = 'MONGO - Connected to ' + _this.dbName;
-        process.env.NODE_ENV == 'development' ?
-            console.log(log) :
-            log;
+    _this.MongoClient.connect(this.uri).then((db) => {
+        let log = 'Find| MONGO - Connected to ' + _this.dbName;
         let dbo = db.db(_this.dbName);
-        dbo.collection(_this.coll).find(what).toArray(function(err, doc) {
+        process.env.NODE_ENV == 'development' ? console.log(log) : log;
+        dbo.collection(_this.coll).find(what).toArray((err, doc) => {
             db.close();
             if (err) {
-                callback && callback(err);
-                throw err;
+                return callback && callback(err);
             } else {
-                callback && callback(doc);
-                return doc;
+                return callback && callback(doc);
             }
         });
-    }).catch(function(err) {
-        if (err) {
-            throw err;
-        }
+    }).catch((err) => {
+        return callback && callback(err);
     });
 };
 
@@ -159,39 +123,26 @@ Crud.prototype.find = function(what, callback) {
  */
 Crud.prototype.update = function(who, what, data, callback) {
     let _this = this;
-    _this.MongoClient.connect(this.uri).then(function(db) {
+    _this.MongoClient.connect(this.uri).then((db) => {
+        let log = 'Update| MONGO - Connected to ' + _this.dbName;
         const dbo = db.db(_this.dbName);
         const uid = new _this.ObjectId(who);
-        const opt = '$push';
         let fuid = {};
-        let fset = {};
-        fset[opt] = {};
-
         fuid['_id'] = uid;
-        fset[opt][what] = data;
-        let log = 'MONGO - Connected to ' + _this.dbName;
-        process.env.NODE_ENV == 'development' ?
-            console.log(log) :
-            log;
-
-        dbo.collection(_this.coll).update(fuid, fset).then(function(result) {
+        let fset = {};
+        fset['$push'] = {};
+        fset['$push'][what] = data;
+        process.env.NODE_ENV == 'development' ? console.log(log) : log;
+        dbo.collection(_this.coll).update(fuid, fset).then((result) => {
             db.close();
-            log = 'MONGO | Inserted data :\n[';
-            log += JSON.stringify(data) + ']';
-            process.env.NODE_ENV == 'development' ?
-                console.log(log) :
-                log;
-            callback && callback(result);
-            return result;
-        }).catch(function(err) {
-            if (err) {
-                throw err;
-            }
+            log = 'MONGO - Inserted :\n[' + JSON.stringify(data) + ']';
+            process.env.NODE_ENV == 'development' ? console.log(log) : log;
+            return callback && callback(result);
+        }).catch((err) => {
+            return callback && callback(err);
         });
-    }).catch(function(err) {
-        if (err) {
-            throw err;
-        }
+    }).catch((err) => {
+        return callback && callback(err);
     });
 };
 
@@ -203,38 +154,28 @@ Crud.prototype.update = function(who, what, data, callback) {
  */
 Crud.prototype.add = function(who, what, data, callback) {
     let _this = this;
-    _this.MongoClient.connect(this.uri).then(function(db) {
+    _this.MongoClient.connect(_this.uri).then((db) => {
+        let log = 'Add| MONGO - Connected to ' + _this.dbName;
         const dbo = db.db(_this.dbName);
         const uid = new ObjectId(who);
-        const opt = '$push';
         let fuid = {};
         let fset = {};
 
-        fset[opt] = {};
+        fset['$push'] = {};
         fuid['_id'] = uid;
-        fset[opt][what] = data;
-        let log = 'MONGO - Connected to ' + _this.dbName;
-        process.env.NODE_ENV == 'development' ?
-            console.log(log) :
-            log;
-        dbo.collection(_this.coll).update(fuid, fset).then(function(result) {
+        fset['$push'][what] = data;
+        process.env.NODE_ENV == 'development' ? console.log(log) : log;
+        dbo.collection(_this.coll).update(fuid, fset).then((result) => {
             db.close();
-            log = 'MONGO | Inserted data :\n[';
-            log += JSON.stringify(data) + ']';
-            process.env.NODE_ENV == 'devdb' ?
-                console.log(log) :
-                log;
+            log = 'MONGO - Inserted :\n[' + JSON.stringify(data) + ']';
+            process.env.NODE_ENV == 'development' ? console.log(log) : log;
             callback && callback(result);
             return result;
-        }).catch(function(err) {
-            if (err) {
-                throw err;
-            }
-        });
-    }).catch(function(err) {
-        if (err) {
+        }).catch((err) => {
             throw err;
-        }
+        });
+    }).catch((err) => {
+        throw err;
     });
 };
 
