0f6215cb2ab387f358ffcc476f663423db8bc0cf
Trevis Gulby <trevisgulby@protonmail.ch>
2018-04-15 14:43:11 +0200

    update nginx conf files with new filepath

--- conf/onion/turnmeon.sh
+++ conf/onion/turnmeon.sh
@@ -6,18 +6,16 @@
 #  /_/  \__,_/_/  /_/ /_/  /_/ /_/ /_/\___/   \____/_/ /_/
 # ---------------------TurnMeOn.sh --------------------
 # Usage : launched by the Dockerfile $ENTRYPOINT for Onion docker service
-cd /usr/src/app && \
-sudo chown -R fofo:fofo /var/lib/tor && \
-sudo chmod -R 700 /var/lib/tor && \
-sudo chown -R fofo:fofo /var/log/tor && \
-sudo chown -R fofo:fofo log tmpdata && \
-sudo chown -R fofo /var/log/nginx && \
-tor && \
-export IP=$(ip -o -4 a s wlan0 | awk '{ print $4 }' | cut -d/ -f1) \
-&& echo "proxy_pass http://$(ip -o -4 a s wlan0 | awk '{ print $4 }' | cut -d/ -f1):3000;" \
-> /usr/src/app/ipview && \
-echo "proxy_pass http://$(ip -o -4 a s wlan0 | awk '{ print $4 }' | cut -d/ -f1):3001;" \
-> /usr/src/app/ipsock && \
-mkdir -p /usr/src/app/DTAFOOD/news && mkdir /usr/src/app/DTAFOOD/prices && \
-sudo nginx && ./INIT_DEV.sh -rl production onion && \
-screen -dmS applog ./node_modules/pm2/bin/pm2 logs
+set +e
+export IP=$(ip -o -4 a s wlan0 | awk '{ print $4 }' | cut -d/ -f1)
+echo "proxy_pass http://$IP:3000;" > /usr/src/app/coin_board/ipview
+echo "proxy_pass http://$IP:3001;" > /usr/src/app/coin_board/ipsock
+sudo chown -R fofo:fofo /var/lib/tor
+sudo chmod -R 700 /var/lib/tor
+sudo chown -R fofo:fofo /var/log/tor
+sudo chown -R fofo:fofo log tmpdata
+sudo chown -R fofo /var/log/nginx
+tor
+mkdir -p /usr/src/app/coin_board/DTAFOOD/{news,prices}
+sudo nginx -t && sudo nginx && ./INIT_DEV.sh -rl production onion
+screen -dmS applog ./usr/src/app/coin_board/node_modules/pm2/bin/pm2 logs
