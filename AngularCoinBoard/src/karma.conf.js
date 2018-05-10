e2c74db433dbb6ed777d56195abf867a1778ae94
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-10 12:47:11 +0200

    add codevoc reporter to karma conf

--- AngularCoinBoard/src/karma.conf.js
+++ AngularCoinBoard/src/karma.conf.js
@@ -1,31 +1,41 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+/** Karma configuration file, see [link](https://karma-runner.github.io/1.0/config/configuration-file.html)
+ * for more information
+ */
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+    config.set({
+        basePath: '../',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        preprocessors: {
+            'src/app/**/*.js': ['coverage']
+        },
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-coverage'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter: {
+            dir: 'coverage/',
+            reports: ['html', 'lcov', 'text'],
+            file: 'coverage.txt',
+            fixWebpackSourcePaths: true
+        },
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/'
+        },
+        reporters: ['progress', 'coverage', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
 };
