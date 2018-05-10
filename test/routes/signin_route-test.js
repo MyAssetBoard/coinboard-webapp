7568a7b28d7244f8278b51769c9da9910cc99722
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-10 17:42:51 +0200

    fixing typo in text desc

--- test/routes/signin_route-test.js
+++ test/routes/signin_route-test.js
@@ -9,28 +9,28 @@ const request = require('supertest');
 const app = require('../../coin_board/controllers/router_methods');
 let testurl = '/signin';
 
-describe('GET index', function() {
-    before(function() {
+describe('GET signin', function () {
+    before(function () {
         return this.spy = sinon.spy(app, 'render');
     });
-    after(function() {
+    after(function () {
         return this.spy.restore();
     });
-    it('should exist', function() {
+    it('should exist', function () {
         return request(app).get('/').expect(200);
     });
-    return it('should render the "index" view', function() {
+    return it('should render the "signin" view', function () {
         return expect(this.spy.getCall(0).args[0]).to.be.eql('page');
     });
 });
 
-describe('GET /unknow', function() {
-    it('it respond with 404', function(done) {
+describe('GET /unknow', function () {
+    it('it respond with 404', function (done) {
         request(app)
             .get(testurl + '/noextsgsg')
             .set('Accept', 'application/json')
             .expect(404)
-            .end(function(err) {
+            .end(function (err) {
                 if (err) return done(err);
                 done();
             });
