52fec9f17e67803f9e566bbdec608d38b23c4ca6
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-30 20:30:19 +0200

    [DeployConf] Update INIT SHell script

--- INIT_DEV.sh
+++ INIT_DEV.sh
@@ -135,6 +135,17 @@ function app_startDev ()
 	"$PM2" start "$APPCONF" --only "$CBSERV"  --update-env;
 }
 
+function app_startStaging ()
+{
+	export NODE_ENV="$1"
+	export SERV_ENV="$2"
+	#Init random key gen for uid
+	echo 'RANDOM' > log.txt;
+	"$PM2" start "$APPCONF" --only "$WSSERV"  --update-env;
+	"$PM2" start "$APPCONF" --only "$WVSERV"  --update-env;
+	"$PM2" start "$APPCONF" --only "$CBSERV"  --update-env;
+}
+
 function reset_db ()
 {
 	rm -rf tmpdata && mkdir tmpdata
@@ -260,6 +271,11 @@ do
 
 		'n-lt')
 		app_tests
+		exit 0
+		;;
+
+		'n-hs')
+		app_startStaging "$2" "$3"
 		exit 0
 		;;
 
